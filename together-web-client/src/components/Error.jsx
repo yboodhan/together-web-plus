6a0c748ba5d1b22e7048d4f04b1ed416c3e4648c
yboodhan <yboodhan@gmail.com>
2020-04-08 15:26:56 -0700

    Completed error page message and svg import

--- together-web-client/src/components/Error.jsx
+++ together-web-client/src/components/Error.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import {ReactComponent as ErrorImage} from '../static/404_page_not_found.svg';
+
+const style = {
+    display: 'flex', 
+    alignItems: 'center', 
+    justifyContent:'center'
+}
 
 const Error = props => {
     return (
-        <div>An error occured.</div>
+        <div>
+            <h1 className="text-center">Oops, this page cannot be found.</h1>
+            <div className="p-5" style={style}>
+                <ErrorImage height={300} width={300}/>
+            </div>
+        </div>
+
     )
 }
 
